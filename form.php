e8a2b58fff82035c887828ffffdab4e2e8683c0e
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-03-17 16:23:17 +0100

    Feat form in form.php added

---
+++ form.php
@@ -0,0 +1,5 @@
+<form action="index.php" method="GET">
+    <input type="text" name="paragrafo" placeholder="Inserisci del testo">
+    <input type="text" name="parola" placeholder="Inserisci parola da censurare">
+    <button type="submit">Invia</button>
+</form>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+
+$paragrafo = $_GET['paragrafo'];
+var_dump($_GET);
+
+?>
+
+<h1>ciao</h1>
+
+<p> <?php echo $paragrafo ?> </p>
